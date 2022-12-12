bf3345338fe91a821414fb4f362b5243c1ab83d1
menen123 <you@example.com>
2022-12-12 11:15:36 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+  
+{
+  int puts (const char* str);
+  char Mystr1[] = "Programming is like building a multilingual puzzle";
+  char Mystr2[] = "e";
+  puts(Mystr1);
+  puts(Mystr2);
+  return (0);
+  
+}
