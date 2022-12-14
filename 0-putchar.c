590127ad7cfb4a83ba6ec229d46a22d68990af91
menen123 <you@example.com>
2022-12-14 06:46:11 -0800

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int ch;
+ for(ch = '_';ch = 'p';ch = 'u');
+ for(ch ='t';ch = 'c';ch = 'h',ch ='a');
+ for(ch = 'r';ch;);
+ putchar(ch);
+ putchar(ch);
+ putchar(ch);
+return(0);
+}
