8503fd88ebec0cd757f305d8b1d628cdd04f48ff
menen123 <you@example.com>
2022-12-15 13:47:04 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -45,3 +45,135 @@ putchar(ch)
 }
 return(0);
 }
+/*4-isalpha.c**/
+
+int _isalpha(int c)
+int ch,isalpha;
+int isalpha()[] =
+if isalpha = 0
+{
+for (ch = '0');
+putchar(ch);
+}
+else
+{
+for(ch = '1')
+putchar(ch);
+}
+return 0;
+}
+/**5.sign*/
+# include <stdio.h>
+int print_sign(int n)
+int n,ch ;
+if(n<0)
+{
+for(ch = '-',ch = '1');
+putchar(ch);
+}
+ifelse(n>0);
+{
+for(ch = '+',ch = '1')
+putchar(ch);
+}else{
+for(ch = 0);
+putchar(ch);
+}
+/**6.6-abs.c*/
+#include <stdio.h>
+#include <math.h>
+#include<stdlib.h>
+int_abs(int)()
+{
+int ch;
+do{
+for(ch >= '0' ;ch <= '0';);
+putchar(ch);
+}
+while (abs(ch) > 0);
+return(0);
+}
+/**7.lastdigi*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int print_last_digit(int)()
+{
+int lastDigit,ch,n;
+while (getchar() ==n )
+{
+lastDigit = n%10;
+}
+for(ch='n';ch%10;);
+putchar(ch);
+return(0);
+}
+/**8.8-24hours.c*/
+#include <stdio.h>
+#include<stdlib.h>
+#include <math.h>
+#include <time.h>
+void jack_bauer(void)
+{
+int T,t,Time;
+while (T <= 23 && t <= 59)
+{
+for(T >= 0;T++;);
+for(t <= 0;t++;);
+putchar('T');
+putchar('T');
+putchar(':');
+putchar('t');
+putchar('t');
+putchar('\n');
+}
+return(0);
+}
+/**9.9-times table*/
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void times_table(void)
+{
+int r;
+int num1 = 0,1,2,3,4,5,6,7,8,9;
+int num2 = 0,1,2,3,4,5,6,7,8,9;
+while(r <= 81)
+{
+r = num1 * num2;
+putchar('r');
+}
+return 0;
+}
+
+
+/**10.10-add.c*/
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int add(int, int)
+{
+int ch;
+for(ch = 'x';ch = 'y';ch+ch);
+putchar(ch);
+return 0;
+}
+
+/**11.1-print_to_98.c*/
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+void print_to_98(int n)
+{
+int ch;
+int n[98] =
+while (n <= 98){
+for(ch >= 0;ch++;);
+getchar()
+putchar('n');
+putchar(',');
+putchar(ch);
+}
+return 0;
+}
