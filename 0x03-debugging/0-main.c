197a0475ac71d02a9008e46710973cccf2f7de59
menen123 <you@example.com>
2022-12-18 07:58:25 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+int main()
+{
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 #pragma once
+{
+void char "positive_or_negative(i)";  
+int i;
+void positive_or_negative(int i);
+if (i > 0)
+return positive_or_negative(i) = i is positive
+else (i < 0)
+return positive_or_negative(i) = i is negative
+}
 int a;
 int b;
 int c; 
-
+#endif
 
